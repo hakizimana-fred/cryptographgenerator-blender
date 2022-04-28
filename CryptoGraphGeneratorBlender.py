9c30905debd41f72ebed92aba6c78baab5504819
hakifred <hakifred20@gmail.com>
2022-04-28 23:51:09 +0300

    got rid of intervals

--- CryptoGraphGeneratorBlender.py
+++ CryptoGraphGeneratorBlender.py
@@ -4,7 +4,7 @@ import requests
 coins = ['ADA', 'XRP', 'LINK', 'SOL', 'XMR']
 width = 1
 coin_spacing = 1.5
-interval = 0
+
 
 # Loop through the list of coins and fetch their prices.
 for i, coin in enumerate(coins):
@@ -22,13 +22,13 @@ for i, coin in enumerate(coins):
         vert.co[0] += i * coin_spacing + 0.5
     # Add Animations
     coin_bar.scale = [0, 0, 0]
-    coin_bar.keyframe_insert(data_path="scale", frame=10 + interval)
+    coin_bar.keyframe_insert(data_path="scale", frame=10)
     coin_bar.scale = [1, 1, 5]
-    coin_bar.keyframe_insert(data_path="scale", frame=50 + interval)
+    coin_bar.keyframe_insert(data_path="scale", frame=50)
     coin_bar.scale = [1, 1, .5]
-    coin_bar.keyframe_insert(data_path="scale", frame=70 + interval)
+    coin_bar.keyframe_insert(data_path="scale", frame=70)
     coin_bar.scale = (width, float(usd_price), 1)
-    coin_bar.keyframe_insert(data_path="scale", frame=80 + interval)
+    coin_bar.keyframe_insert(data_path="scale", frame=80)
 
     # Add text
     bpy.ops.object.text_add()
@@ -37,4 +37,5 @@ for i, coin in enumerate(coins):
     bpy.ops.transform.rotate(value=1.5708)
     bpy.ops.transform.translate(value=(i*coin_spacing+0.5, -0.5, 0))
     bpy.context.object.data.body = f"{coin} {usd_price}"
-    interval += 1
+   
+    
